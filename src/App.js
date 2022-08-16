98c7107a98b763dcc1b4c9ed1288cedd03897d13
Nishant1242 <nishantkadam1242@gmial.com>
2022-08-16 20:02:19 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Switch>
-      <Route path="/">This is home page</Route>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route path="/starred">This is starred</Route>
+      <Route>404 not found</Route>
     </Switch>
   );
 }
